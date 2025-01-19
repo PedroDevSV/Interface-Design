ff4e3a88126d515b8356006a119f18c4ce803b48
Periko_elmaki <pedro.sandoval.martinez@ces-vegamedia.es>
2025-01-19 18:02:03 +0100

    terminado falta memoria

--- RollingStones/assets/animaciones.js
+++ RollingStones/assets/animaciones.js
@@ -12,12 +12,12 @@ window.addEventListener('scroll', function () {
         nav.style.position = 'relative';
         nav.style.marginTop = '0';
         nav.style.width = '40%';
-        nav.style.zIndex = '1'; // Añadir esta línea
+        nav.style.zIndex = '1'; 
     }
 });
 
 // Función para reproducir el audio de las imagenes 
-function reproducirAudio(imageId, buttonId, audioId) {
+function audioCollage(imageId, buttonId, audioId) {
     const audio = document.getElementById(audioId);
     const image = document.getElementById(imageId);
     const playButton = document.getElementById(buttonId);
@@ -27,7 +27,7 @@ function reproducirAudio(imageId, buttonId, audioId) {
 
     // click en el botón de reproducción
     playButton.click();
-    // Añadir evento de hover a la imagen para cambiar la opacidad
+    //cambiar la opacidad
     image.addEventListener('mouseover', function () {
         image.style.opacity = '0.5';
     });
@@ -36,16 +36,20 @@ function reproducirAudio(imageId, buttonId, audioId) {
         image.style.opacity = '1';
     });
 }
+// LLamada botones de imagenes
+document.getElementById('play-button-collage2').addEventListener('click', function () {
+    document.getElementById('audio').play();
+});
 
+// Ejecutar para collage2
+reproducirAudio('collage2', 'play-button-collage2', 'audio');
 
 // Para reproducir audio en hover
 var currentAudio = null;
 
 function playAudio(audioId) {
-    // if (currentAudio && !currentAudio.ended) {
-    //     return; // No reproducir si ya hay un audio reproduciéndose
-    // }
-    // Detener el audio actual si hay uno reproduciéndose
+   
+    // COntrolamos que se detenga el audio actual si hay uno reproduciéndose
     if (currentAudio && !currentAudio.paused) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
@@ -54,11 +58,7 @@ function playAudio(audioId) {
     currentAudio.play();
 }
 
-// LLamada botones de imagenes
-document.getElementById('play-button-collage2').addEventListener('click', function () {
-    document.getElementById('audio').play();
-});
 
-// Ejecutar para collage2
-reproducirAudio('collage2', 'play-button-collage2', 'audio');
+
+
 
