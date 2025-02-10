2ec7a456463d6aac3d448c8a96437223c9322b88
Periko_elmaki <pedro.sandoval.martinez@ces-vegamedia.es>
2025-02-11 00:19:27 +0100

    casi terminado

--- RollingStones/assets/animaciones.js
+++ RollingStones/assets/animaciones.js
@@ -4,33 +4,41 @@ window.addEventListener('scroll', function () {
     if (window.scrollY > 120) {
         nav.style.position = 'fixed';
         nav.style.top = '0';
-        nav.style.width = '27%';
+        nav.style.width = '400px';
         nav.style.marginTop = '10px';
-        nav.style.fontSize='0.9em';
-        nav.style.transition='0.5s ease';
+        nav.style.fontSize = '10pt';
         //sobreponer nav 
         nav.style.zIndex = '1000'; 
     } else {
         nav.style.position = 'relative';
         nav.style.marginTop = '0';
-        nav.style.width = '40%';
-        nav.style.zIndex = '1'; 
         nav.style.fontSize = '12pt';
+        nav.style.width = '500px';
+        nav.style.zIndex = '16pt'; // Añadir esta línea
     }
 });
 
+// Para reproducir audio en hover
+var currentAudio = null;
+
 // Función para reproducir el audio de las imagenes 
-function audioCollage(imageId, buttonId, audioId) {
+function reproducirAudio(imageId, buttonId, audioId) {
     const audio = document.getElementById(audioId);
     const image = document.getElementById(imageId);
     const playButton = document.getElementById(buttonId);
+
+    // Detener el audio actual si hay uno reproduciéndose
+    if (currentAudio && !currentAudio.paused) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
 
     // hover sobre la imagen
     image.dispatchEvent(new Event('mouseover'));
 
     // click en el botón de reproducción
     playButton.click();
-    //cambiar la opacidad
+    // Añadir evento de hover a la imagen para cambiar la opacidad
     image.addEventListener('mouseover', function () {
         image.style.opacity = '0.5';
     });
@@ -38,21 +46,24 @@ function audioCollage(imageId, buttonId, audioId) {
     image.addEventListener('mouseout', function () {
         image.style.opacity = '1';
     });
+
+    // Asignar el nuevo audio a currentAudio
+    currentAudio = audio;
 }
+
+
 // LLamada botones de imagenes
 document.getElementById('play-button-collage2').addEventListener('click', function () {
     document.getElementById('audio').play();
 });
 
 // Ejecutar para collage2
-// reproducirAudio('collage2', 'play-button-collage2', 'audio');
+reproducirAudio('collage2', 'play-button-collage2', 'audio');
+
 
 // Para reproducir audio en hover
-var currentAudio = null;
-
 function playAudio(audioId) {
-   
-    // COntrolamos que se detenga el audio actual si hay uno reproduciéndose
+    // Detener el audio actual si hay uno reproduciéndose
     if (currentAudio && !currentAudio.paused) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
@@ -60,8 +71,3 @@ function playAudio(audioId) {
     currentAudio = document.getElementById(audioId);
     currentAudio.play();
 }
-
-
-
-
-
